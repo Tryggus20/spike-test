9b9e4f4cbc94e24124d955675df6b652c4afaddb
David Campbell <tryggus20@gmail.com>
2023-10-15 16:05:26 -0500

    get request for specific user works

--- server/routes/concert.router.js
+++ server/routes/concert.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+//gt request for all users
 router.get("/", (req, res) => {
   const query = `SELECT "users".id, "users".username,
  "concerts".venue, "concerts".date, ARRAY_AGG("bands".name
@@ -10,12 +11,43 @@ router.get("/", (req, res) => {
    JOIN "concerts" ON "concerts".id = "user_concerts".id JOIN 
 "bands" ON "bands".concert_id = "concerts".id GROUP BY "users".id,
 "users".username, "concerts".venue, "concerts".date;
-`;
+`; // ARRAY_AGG does change bands into an array of strings!
 pool.query(query).then((result) => {
     res.send(result.rows);
 }).catch((err) => {
-    console.log("error getting all concerts for every movie,", err);
+    console.log("error getting all concerts for everyone,", err);
     res.sendStatus(500);
 });
-})
+})// end of GET request for ALL CONCERTS
+
+//get request for single user!
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const query = `SELECT "users".id AS user_id,
+     "users".username, "concerts".venue, "concerts".date,
+      ARRAY_AGG("bands".name ORDER BY "bands".concert_position ASC)
+       FROM "users" JOIN "user_concerts" ON "user_concerts".id 
+       = "users".id JOIN "concerts" ON "concerts".id = 
+       "user_concerts".id JOIN "bands" ON "bands".concert_id = 
+       "concerts".id WHERE "user_id"=$1 GROUP BY "users".id, 
+       "users".username, "concerts".venue, "concerts".date;`;
+       //// ARRAY_AGG does change bands into an array of strings!
+  pool.query(query, [id]).then((result) => {
+      res.send(result.rows);
+  }).catch((err) => {
+      console.log("error getting all concerts for single user,", err);
+      res.sendStatus(500);
+  });
+  })// end of GET request for a SPECIFIC USER'S CONCERTS
+  
+
+  router.post("/:id", (req, res) => {
+const id= req.params.id;
+
+  })
+
+
+
 module.exports = router;
+
+
