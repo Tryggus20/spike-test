896767d3e2d155b98017d9f309906e7c58924be2
David Campbell <tryggus20@gmail.com>
2023-10-15 15:31:20 -0500

    db sql file changed and pool changed to correct db

--- database.sql
+++ database.sql
@@ -1,10 +1,65 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+-- create database concert_masterlist
+-- MAY NEED TO CHANGE SOME STUFF TO INCLUDE FOREIGN KEY!!!
+--USERS TABLE
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(30) not null,
+is_admin BOOLEAN default false,
+password VARCHAR(30) not null
 );
+
+
+-- thoughts on venue/city/state not null?
+--CONCERTS TABLE
+CREATE TABLE concerts (
+id SERIAL PRIMARY KEY,
+venue VARCHAR(60),
+city VARCHAR(30),
+state VARCHAR(2),
+date DATE NOT NULL
+);
+
+--BANDS TABLE
+CREATE TABLE bands (
+id SERIAL PRIMARY KEY,
+concert_id INTEGER REFERENCES concerts,
+name VARCHAR(60) NOT NULL,
+concert_position INTEGER NOT NULL
+);
+
+--USER_CONCERTS Joiner table
+CREATE TABLE user_concerts (
+id SERIAL PRIMARY KEY,
+user_id INTEGER REFERENCES users,
+concert_id INTEGER REFERENCES concerts,
+hidden BOOLEAN default false,
+is_deleted BOOLEAN default false,
+deleted_date DATE,
+favorite BOOLEAN default false,
+comments VARCHAR(280),
+seat_location VARCHAR(30)
+);
+
+--ENHANCEMENTS
+CREATE TABLE enhancements (
+id SERIAL PRIMARY KEY,
+user_concert_id INTEGER REFERENCES user_concerts,
+type VARCHAR(30) NOT NULL,
+description VARCHAR(100)
+);
+
+-- FRIENDS 
+CREATE TABLE friends (
+id SERIAL PRIMARY KEY,
+user_1 INTEGER REFERENCES users,
+user_2 INTEGER REFERENCES users,
+follow BOOLEAN default false,
+met_where VARCHAR(60)
+);
+
+
+select * FROM "users";
+
+
+
+SELECT "users".id, "users".username, "concerts".venue, "concerts".date, ARRAY_AGG("bands".name ORDER BY "bands".concert_position ASC) FROM "users" JOIN "user_concerts" ON "user_concerts".id = "users".id JOIN "concerts" ON "concerts".id = "user_concerts".id JOIN "bands" ON "bands".concert_id = "concerts".id GROUP BY "users".id, "users".username, "concerts".venue, "concerts".date;
